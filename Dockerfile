b22c8b730975e0516ff13dfe71e42582370d8b77
high_byte <email@hanan.beer>
2023-05-15 19:59:38 +0300

    fail

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,2 @@
-# install foundry
-FROM ubuntu:20.04
-RUN apt-get update
-RUN apt-get install -y bash curl gcc git nginx
-#ENV SHELL=/bin/bash
-SHELL ["/bin/bash", "-c"]
-RUN curl -kL https://foundry.paradigm.xyz | bash
-RUN ~/.foundry/bin/foundryup
-EXPOSE 8545
-RUN ~/.foundry/bin/anvil
-
-# install nginx
-#FROM nginx:alpine
-#COPY site /usr/share/nginx/html
-
-#ENTRYPOINT ["bash"]
+FROM nginx:alpine
+COPY site /usr/share/nginx/html
--- site/main.js
+++ site/main.js
@@ -21,7 +21,8 @@ let steth
 let browser_provider = new ethers.BrowserProvider(window.ethereum)
 
 // const rpc_url = 'http://127.0.0.1:8545/'
-const rpc_url = `https://${window.location.hostname}:8545/`
+// TODO: setup dedicated anvil env
+const rpc_url = 'https://97a3-147-235-197-42.ngrok-free.app'
 const rpc = new ethers.JsonRpcProvider(rpc_url)
 const empty_wallet = new ethers.Wallet(PRIVATE_KEY, rpc)
 let user_wallet
@@ -63,9 +64,15 @@ function setDemoText(html) {
     el_demo.innerHTML = html
 }
 
+function setManaText(receipt) {
+    // let mana_price = receipt.gasPrice
+    let mana_str = parseInt(((mana_price * receipt.gasUsed) * 1851n) / E15) / 1000
+    el_mana.innerText = `txn price: $${mana_str}`
+}
+
 async function updateItem(index, delta_amount, use_wallet=false) {
     if (index < 0 || index > items.length || index >= 2**TREE_HEIGHT) {
-        alert('bad index')
+        alert('bad index\n(note: new deposits must be the next index which is ' + items.length + ')')
         return false
     }
 
@@ -86,8 +93,8 @@ async function updateItem(index, delta_amount, use_wallet=false) {
     let localRoot = BigInt(tree[TREE_HEIGHT][0])
     let chainRoot = await lottoyield.$rootHash()
     if (localRoot != chainRoot) {
-        updateRewardPool()
-        setDemoText('D E M O - please wait for transactions to finish\n(some wallets lose track of transactions, let me refresh that for you)')
+        loadStakesFromStorage()
+        setDemoText('D E M O - please wait for transactions to finish')
         throw new Error('root mismatch')
     }
 
@@ -119,6 +126,7 @@ async function updateItem(index, delta_amount, use_wallet=false) {
     setDemoText(`sent: ${resp.hash}`)
 
     let receipt = await rpc.waitForTransaction(resp.hash)
+    setManaText(receipt)
     
     setDemoText(`receipt: ${receipt.logs.length} logs`)
 
@@ -143,13 +151,13 @@ function un18f2(n) {
     return f2(un18(n))
 }
 
+const eth_price = 1851
+const mana_price = 9746782242n
 function eth2usd(eth) {
-    const eth_price = 1851
     return un18(BigInt(eth) * BigInt(eth_price) * 100n) / eth_price
 }
 
-function eth2usdstr(eth) {
-    const eth_price = 1851
+function smoleth2usdstr(eth) {
     let usd = parseFloat(eth) * eth_price
     return '$' + f2(usd)
 }
@@ -310,6 +318,8 @@ async function testEndRound() {
     let res = await lottoyield.nextRound()
     setDemoText(`sent: ${res.hash}`)
     let receipt = await rpc.waitForTransaction(res.hash)
+    setManaText(receipt)
+
     let log = receipt.logs[0]
     let [root_hash, random, reward] = encoder.decode(['uint256','uint128','uint128'], log.data)
     root_hash = root_hash.toString(16)
@@ -350,9 +360,16 @@ async function testClaimWin() {
     let tree = merklizeItems(items, TREE_HEIGHT)
     let proof = getProof(tree, idx)
     try {
-        await lottoyield.claimReward(round_id, winner_idx, items[idx].balance, items[idx].shares, proof)
+        let res = await lottoyield.claimReward(round_id, winner_idx, items[idx].balance, items[idx].shares, proof)
+        let receipt = await rpc.waitForTransaction(res.hash)
+        setManaText(receipt)
         setDemoText('reward claimed (0 schmekels for demo)')
     } catch (err) {
+        if (err.reason == 'invalid claim proof') {
+            testEndRound()
+            alert('demo does not yet implement reading events from chain.\n(check again tomorrow)\nfor now, claim rewards immediately after round end.\nlet me do that for you...')
+            return
+        }
         alert(err)
     }
 }
